e1671f3695922bee3c3b8680c62466b67cfa39f3
meinhoonharsh <harshprogrammer782@gmail.com>
2023-09-28 03:07:52 +0530

    first commit

---
+++ src/Home.js
@@ -0,0 +1,156 @@
+import React, { useState } from "react";
+import jsondata from "./data.json";
+import image from "./jeetpakki.png";
+import toast, { Toaster } from "react-hot-toast";
+import { InfinitySpin } from "react-loader-spinner";
+
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+import axios from "axios";
+import Slides from "./Slides";
+
+export default function Home() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [slidesCount, setSliidesCount] = useState(5);
+  const [prompt, setPrompt] = useState("");
+  const [showSlides, setShowSlides] = useState(false);
+  const [data, setData] = useState(jsondata);
+
+  const handleLearnClick = () => {
+    setIsLoading(true);
+    setShowSlides(false);
+    toast("Generating Slides");
+    const token = process.env.REACT_APP_API_KEY;
+    const newprompt = `Suppose you are a School Teacher, Explain ${prompt} to your Student
+    Explain it with the help of ${slidesCount} points, Each should not be more than 25-30 words
+
+    Write Image generation prompts for each point, image should not contain any text
+
+    return your answer in the form
+    {
+      "points":[
+         "point one",
+         "point two",
+         "point three"
+    ],
+      "prompts_for_images":[
+         "prompt for point one",
+         "prompt for point two",
+         "prompt for point three"
+    ]
+    }
+    `;
+    axios
+      .post(
+        "https://api.openai.com/v1/completions",
+
+        {
+          model: "text-davinci-003",
+          prompt: newprompt,
+          temperature: 0.7,
+          max_tokens: 256,
+          top_p: 1,
+          frequency_penalty: 0,
+          presence_penalty: 0,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + token,
+          },
+        }
+      )
+      .then((res) => {
+        toast.success("Slides Generated");
+        console.log(res.data);
+        const output = res.data.choices[0].text;
+
+        // trim before first { and after last }
+        const start = output.indexOf("{");
+        const end = output.lastIndexOf("}");
+        const trimmed = output.substring(start, end + 1);
+        console.log(trimmed);
+
+        const parsed = JSON.parse(trimmed);
+        setData(parsed);
+        setShowSlides(true);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        toast.error("OOOps! something went wrong ");
+        setIsLoading(false);
+      });
+
+    // setData(jsondata);
+  };
+
+  return (
+    <div>
+      <Toaster />
+      <div className="navbar">
+        <div className="logo">
+          <img src="/logo.png" style={{ width: 40 }} />
+          &nbsp; Kwikie
+        </div>
+        <div className="nav-links">
+          <a
+            href="https://github.com/meinhoonharsh/rajhack/"
+            target="_blank"
+            class="button"
+          >
+            Github
+          </a>
+        </div>
+      </div>
+
+      <div className="hero">
+        <div className="hero-left">
+          <div className="hero-text">
+            <h1>
+              Understand <span className="yellowspan">Any Topic</span> <br /> in
+              just <span className="pinkspan">One</span> Click
+            </h1>
+          </div>
+          {/* Input with button */}
+          {isLoading ? (
+            <InfinitySpin width="200" color="#fff" />
+          ) : (
+            <div className="input-holder">
+              <input
+                type="text"
+                value={prompt}
+                onChange={(e) => setPrompt(e.target.value)}
+                placeholder="Search for a topic"
+              />
+              {/* Select for number of Slides */}
+              <select
+                value={slidesCount}
+                onChange={(e) => setSliidesCount(e.target.value)}
+                id="slides"
+              >
+                <option value="3">3</option>
+                <option value="5" selected>
+                  5
+                </option>
+                <option value="7">7</option>
+                <option value="10">10</option>
+              </select>
+
+              <button className="search-button" onClick={handleLearnClick}>
+                Get Kwik <br />
+                Gyan!
+              </button>
+            </div>
+          )}
+        </div>
+        {/* <div className="hero-right">
+          <img src={image} />
+        </div> */}
+      </div>
+
+      {/* Slides */}
+      {showSlides && <Slides data={data} />}
+    </div>
+  );
+}
---
+++ src/Slides.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+
+export default function Slides({ data }) {
+  var images = [];
+  console.log("Data from slides.js", data);
+  const getImageFromPrompt = async (prompt) => {
+    axios
+      .post("http://stablediffusionapi.com/api/v3/text2img", {
+        key: "izBBAFO0G84bXNB2Vh7KR0kYPirfEZzYgaBAci5LYRqINDf5IVN1eohPBJSt",
+        prompt:
+          "Kid eating an apple under the tree, in bright sunny day with a little cloud",
+        negative_prompt:
+          "((out of frame)), ((extra fingers)), mutated hands, ((poorly drawn hands)), ((poorly drawn face)), (((mutation))), (((deformed))), (((tiling))), ((naked)), ((tile)), ((fleshpile)), ((ugly)), (((abstract))), blurry, ((bad anatomy)), ((bad proportions)), ((extra limbs)), cloned face, (((skinny))), glitchy, ((extra breasts)), ((double torso)), ((extra arms)), ((extra hands)), ((mangled fingers)), ((missing breasts)), (missing lips), ((ugly face)), ((fat)), ((extra legs)), anime",
+        width: "512",
+        height: "512",
+        samples: "1",
+        num_inference_steps: "20",
+        seed: null,
+        guidance_scale: 7.5,
+        safety_checker: "yes",
+        webhook: null,
+        track_id: null,
+      })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  // React.useEffect(() => {
+  //   console.log("Data from slides.js", data);
+  //   data.prompts_for_images.forEach((prompt) => {
+  //     console.log(prompt);
+  //     images.push(getImageFromPrompt(prompt));
+  //   });
+  // });
+  return (
+    <div className="slides">
+      <Carousel
+        showArrows={true}
+        autoPlay={true}
+        infiniteLoop={false}
+        showThumbs={true}
+        emulateTouch={true}
+        showStatus={false}
+      >
+        {data.points.map((item) => {
+          return (
+            <div>
+              <div className="slide layout1">
+                <div className="slide-text">{item}</div>
+                {/* <div className="slide-image">
+                  <img src="https://pub-8b49af329fae499aa563997f5d4068a4.r2.dev/generations/26e83d8b-6c01-4b5b-8cdc-8ab337939830-0.png" />
+                </div> */}
+              </div>
+            </div>
+          );
+        })}
+      </Carousel>
+    </div>
+  );
+}
